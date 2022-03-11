f1d03f597a6c7f1fd21a272437bd7a2deb9df1c3
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-03-11 09:36:25 -0500

    feat(engine): updated engine to be more efficient

--- src/app/Engine.java
+++ src/app/Engine.java
@@ -29,62 +29,30 @@ public class Engine {
     // either end
     Node head = list.getHead(), tail = list.getTail();
 
-    // if the list is empty, the head does not exist
+    // if the head is null (nothing in the list), return true because an empty
+    // set of characters is technically a palindrome
     if (head == null)
-      return false;
-
-    // if there's only one element in the linked list, the head and the tail are
-    // the same
-    if (head == tail)
       return true;
 
-    /*
-     * The idea for the algorithm is to start at either end of the list, and
-     * calculate the max number of characters in either direction we will have
-     * to check to ensure this is a palindrome. This number is length of the
-     * palindrome / 2, no matter if the palindrome is even digits or odd. We
-     * will keep a current number of checks, and continue checking inwards until
-     * when we are about to exceed the max amount of checks. If we successfully
-     * make it to the middle, the list is a palindrome. If not, and we find two
-     * characters that are the same amount of characters away from either end of
-     * the list but are not the same, the list is not a palindrome and we return
-     * false.
+    /* The idea for the algorithm is to start at the head and the tail, and keep
+     * moving them further towards the center of the list until they equal each
+     * other. Along the way, we will check if each pair of nodes (which are the
+     * same distance from either end of the word) are the same.
      */
 
-    // calculate the max checks and intitialize the current checks at 0
-    final int maxChecks = list.getLength() / 2;
-    int currentChecks = 0;
+    // while the head does not equal the tail
+    while (head != tail) {
+      if (head.getData() !=
+          tail.getData()) // if the pair of nodes do not hold the same value
+        return false; // return false to signify this list does not contain a
+                      // palindrome
 
-    // while we have not execeeded the max number of checks
-    while (currentChecks <= maxChecks) {
-      // if the two characters the same distance from either end of the
-      // palindrome are not the same, return false
-      if (!(head.getData() == tail.getData()))
-        return false;
-
-      head = head.getNext(); // the pointer that was initially head needs to
-                             // move to the next node
-      tail = tail.getPrev(); // the pointer that was initially head needs to
-                             // move to the previous node
-
-      currentChecks++; // add one to the current checks
+      head = head.getNext(); // move the head variable to the next node
+      tail = tail.getPrev(); // move the tail variable to the previous node
     }
 
-    return true; // if we reach the middle of the list without any problems, the
-                 // list contains a palindrome
-  }
-
-  /**
-   * This method checks if the character given is a special character
-   * (anything that's not a lowercase, capital, or a number)
-   *
-   * @param s - the string to check (will be a character in this case)
-   * @return true if the string is special, else false
-   */
-  private static boolean isSpecialCharacter(final String s) {
-    return (s == null) ? false
-                       : s.matches("[^A-Za-z0-9]"); // use regex to check the
-                                                    // condition described above
+    return true; // if we manage to traverse the entire list, return true to
+                 // signify that this list does contain a palindrome
   }
 
   /**
@@ -97,21 +65,17 @@ public class Engine {
   public static LinkedList generateLinkedList(final String text) {
     final LinkedList list =
         new LinkedList(); // create a new blank linked list to populate
-    final String lowerText =
-        text.toLowerCase(); // make the text lowercase to prevent
-                            // errors due to capitalization
+    final String fixedText =
+        text.replaceAll("[^A-Za-z0-9_-]", "")
+            .toLowerCase(); // get rid of all special characters and spaces in
+                            // the string and store it in a new fixedText
+                            // variable
 
-    // traverse indexes of the string starting from the end and going to the
-    // beginning (backwards). This is because the linked list add method adds
-    // to the head, not to the tail (see linkedList/LinkedList class)
-    for (int index = text.length() - 1; index >= 0; index--) {
-      // if the character at the current index is NOT a specical character
-      if (!isSpecialCharacter(String.valueOf(text.charAt(index)))) {
-        final Node letterNode = new Node(lowerText.charAt(
-            index)); // create a node with the character as the data
-        list.addToHead(letterNode); // add the newly created node to the head of
-                                    // the linked list
-      }
+    // for each character in the fixed text, add the character to the linked
+    // list (in a backwards order, due to the fact that the list only adds to
+    // the head and not the tail)
+    for (int index = fixedText.length() - 1; index >= 0; index--) {
+      list.addToHead(new Node(fixedText.charAt(index)));
     }
 
     return list; // return the newly formed list
